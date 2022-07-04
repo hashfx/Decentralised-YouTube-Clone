cc06b1e8b3d586bc22004bffe441e59e42479d9a
hashfx <algebra.soni@gmail.com>
2022-07-04 16:35:20 +0530

    spacing to 4tab; DVideo.sol to Dvdo.sol

---
+++ src/contracts/Dvdo.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.0;
+
+contract Dvdo {
+    uint256 public videoCount = 0;
+    string public name = "Dvdo";
+
+    //Create id=>struct mapping
+
+    //Create Struct
+
+    //Create Event
+
+    constructor() public {}
+
+    // save video to blockchain
+    function uploadVideo(string memory _videoHash, string memory _title)
+        public
+    {
+        // Make sure the video hash exists
+        // Make sure video title exists
+        // Make sure uploader address exists
+        // Increment video id
+        // Add video to the contract
+        // Trigger an event
+    }
+}
